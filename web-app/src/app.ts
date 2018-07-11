1a4d0c7e9dac07ead2a5d668c58a1bc293d3f541
Karandeep Marwah <kmarwah@pietech.com>
2018-07-11 13:14:20 -0400

    router issues

--- web-app/src/app.ts
+++ web-app/src/app.ts
@@ -1,12 +1,14 @@
+import {PLATFORM} from 'aurelia-pal';
 import {Router} from 'aurelia-router';
 
 export class App {
   router: Router;
   routerConfig(config, router){
     config.title = 'Aurelia Router';
+    config.options.pushState = true;
     config.map([
-      {route: '', name: 'home', moduleId: 'form/form', nav: true, title: 'Home'},
-      {route: 'results', name: 'results', moduleId: 'results/results', nav: true, title: 'Results'}
+      {route: ['', 'form'], name: 'home', moduleId: PLATFORM.moduleName('./components/input-formfield/form.html'), nav: true, title: 'Home'},
+      {route: 'results', name: 'results', moduleId: PLATFORM.moduleName('./components/results/results.html'), nav: true, title: 'Results'}
     ]);
     this.router = router;
   }
