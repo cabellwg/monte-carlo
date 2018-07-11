1902cff48da4a9c2a1781c31d0e833cf56d7d21f
Karandeep Marwah <kmarwah@pietech.com>
2018-07-11 10:34:40 -0400

    router in process

--- web-app/src/app.ts
+++ web-app/src/app.ts
@@ -1,3 +1,16 @@
+import {PLATFORM} from 'aurelia-pal';
+import {RouterConfiguration, Router} from 'aurelia-router';
+import {Script} from 'vm';
+
 export class App {
-  message = 'Hello World!';
+  router:Router;
+  routerConfiguration(config, router){
+    config.title= 'Monte Carlo';
+    config.options.pushState = true;
+    config.map([
+      {route: ['','form'], name: 'form', moduleId: PLATFORM.moduleName('form/form'), nav: true, title: 'Home'},
+      {route: 'results', name: 'results', moduleId: PLATFORM.moduleName('results/results'), nav: true, title: 'Results'}
+    ]);
+    this.router = router;
+  }
 }
