1a7a952a973645ace75900ebb6409bb8a5695a71
Karandeep Marwah <kmarwah@pietech.com>
2018-07-12 16:03:09 -0400

    form input fields changed

--- web-app/src/components/input-formfield/form.ts
+++ web-app/src/components/input-formfield/form.ts
@@ -1,28 +1,7 @@
-import{inject, NewInstance} from 'aurelia-dependency-injection';
-import{ValidationController, ValidationRules} from 'aurelia-validation';
-import {required, email} from 'aurelia-validatejs';
 
-@inject(NewInstance.of(ValidationController))
 export class Form{
 
-  controller;
-  constructor(controller){
-    this.controller = controller;
-  }
-
-  @required
-  validatelabel1 = '';
-
-
- submitFormButton(){
-   let errors = this.controller.validate();
-   
-
-
+submitFormButton(){
    window.location.href="/results"
- } 
-
+  }
 }
-ValidationRules
-  .ensure('validatelabel1').required()
-  .on(this);
