8aa3783209d0029fbcccadd8505ffaed5a7c4f26
Karandeep Marwah <kmarwah@pietech.com>
2018-07-18 15:09:01 -0400

    inputs page form re-design done, now working on form validation

--- web-app/src/components/input-formfield/form.ts
+++ web-app/src/components/input-formfield/form.ts
@@ -1,5 +1,22 @@
+import {inject} from 'aurelia-dependency-injection';
+import {ValidationControllerFactory, ValidationController, ValidationRules, validateTrigger} from 'aurelia-validation';
+import {BootstrapFormRenderer} from './bootstrap-form-renderer';
 
+@inject(ValidationControllerFactory)
 export class Form{
+
+  currentAge = '';
+  retireAge = '';
+  controller = null;
+
+  constructor(controllerFactory){
+    this.controller = controllerFactory.createForCurrentScope();
+    this.controller.addRenderer(new BootstrapFormRenderer());
+    this.controller.validateTrigger= validateTrigger.change;
+  }
+
+
+
 
 submitFormButton(){
    window.location.href="/results"
