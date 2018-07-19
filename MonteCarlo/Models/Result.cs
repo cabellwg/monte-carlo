7be93462856c0c4e3de9cf9bdbd321a73bba640d
William Cabell <wcabell@pietech.com>
2018-07-19 16:11:11 -0400

    Added rate frequencies

--- MonteCarlo/Models/Result.cs
+++ MonteCarlo/Models/Result.cs
@@ -5,6 +5,11 @@ namespace MonteCarlo.Models
     public class Result
     {
         public int SuccessRate { get; set; }
-        public IEnumerable<double[]> PortfolioPercentiles;
+
+        public IEnumerable<double[]> PortfolioPercentiles { get; set; }
+
+        public int[] ReturnRateFrequencies { get; set; }
+        public double FrequencyPeak { get; set; }
+        public double FrequencyScale { get; set; }
     }
 }
