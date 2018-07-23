1793ea7243b99e4c46ff832e86a451dcf9d86478
William Cabell <wcabell@pietech.com>
2018-07-23 11:19:05 -0400

    Added a button group

--- MonteCarlo/Models/Result.cs
+++ MonteCarlo/Models/Result.cs
@@ -12,20 +12,20 @@ namespace MonteCarlo.Models
 
 
         // Histograms
-        public int[] StocksReturnRateFrequencies { get; set; }
+        public int[] StocksReturnRateFrequencies { get; set; } = new int[20];
         public double StocksFrequencyPeak { get; set; }
         public double StocksFrequencyScale { get; set; }
 
-        public int[] BondsReturnRateFrequencies { get; set; }
+        public int[] BondsReturnRateFrequencies { get; set; } = new int[20];
         public double BondsFrequencyPeak { get; set; }
         public double BondsFrequencyScale { get; set; }
 
 
         // Stacked bar
-        public List<double> StocksRetirementAmount { get; set; }
-        public List<double> StocksEndAmount { get; set; }
+        public List<double> StocksRetirementAmount { get; set; } = new List<double>();
+        public List<double> StocksEndAmount { get; set; } = new List<double>();
 
-        public List<double> BondsRetirementAmount { get; set; }
-        public List<double> BondsEndAmount { get; set; }
+        public List<double> BondsRetirementAmount { get; set; } = new List<double>();
+        public List<double> BondsEndAmount { get; set; } = new List<double>();
     }
 }
